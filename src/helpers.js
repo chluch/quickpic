eacf0ed9ad60f2d840a41520050a5bcaf6c38621
Debbie Cheng <dqrcheng@gmail.com>
2020-10-13 22:02:58 +1100

    from merge

--- src/helpers.js
+++ src/helpers.js
@@ -28,66 +28,10 @@ export function fileToDataUrl(file) {
     return dataUrlPromise;
 }
 
-
 // Render HTML code blocks
 export function renderHTML(htmlBlock, elementID) {
     const parser = new DOMParser();
     const newNode = parser.parseFromString(htmlBlock, "text/html");
     const element = newNode.getElementById(elementID);
     document.getElementById("main").appendChild(element);
-}
-
-/**
- * Given a post, return a tile with the relevant data
- * @param   {object}        post 
- * @returns {HTMLElement}
- */
-export function createPostTile(post) {
-    const section = createElement('section', null, { class: 'post' });
-
-    section.appendChild(createElement('h2', post.meta.author, { class: 'post-title' }));
-
-    section.appendChild(createElement('img', null,
-        { src: '/images/' + post.src, alt: post.meta.description_text, class: 'post-image' }));
-
-    return section;
-}
-
-// Given an input element of type=file, grab the data uploaded for use
-export function uploadImage(event) {
-    const [file] = event.target.files;
-
-    const validFileTypes = ['image/jpeg', 'image/png', 'image/jpg']
-    const valid = validFileTypes.find(type => type === file.type);
-
-    // bad data, let's walk away
-    if (!valid)
-        return false;
-
-    // if we get here we have a valid image
-    const reader = new FileReader();
-
-    reader.onload = (e) => {
-        // do something with the data result
-        const dataURL = e.target.result;
-        const image = createElement('img', null, { src: dataURL });
-        document.body.appendChild(image);
-    };
-
-    // this returns a base64 image
-    reader.readAsDataURL(file);
-}
-
-/* 
-    Reminder about localStorage
-    window.localStorage.setItem('AUTH_KEY', someKey);
-    window.localStorage.getItem('AUTH_KEY');
-    localStorage.clear()
-*/
-export function checkStore(key) {
-    if (window.localStorage)
-        return window.localStorage.getItem(key)
-    else
-        return null
-
 }
