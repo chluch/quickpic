9a2119ec3f1f6ffb6c60628742e9a621ff82841b
Debbie Cheng <dqrcheng@gmail.com>
2020-10-13 21:52:04 +1100

    Separate JS for each component

---
+++ src/login.js
@@ -0,0 +1,57 @@
+"use strict";
+import API from "./api.js";
+import { renderHTML } from "./helpers.js";
+
+const loginPage = {
+    load: () => renderHTML(`
+    <form id="login">
+        <div>
+            <input id="username" type="text" placeholder="Please enter your username">
+        </div>
+        <div>
+            <input id="password" type="password" placeholder="Please enter your password">
+        </div>
+        <div class="button-wrapper">
+            <button id="go-to-signup">Signup</button>
+            <button type="submit" id="login-btn">Submit</button>
+        </div>
+    </form>
+    `
+        , "login"),
+    setEvents: () => {
+        document.getElementById("go-to-signup").onclick = (e) => {
+            e.preventDefault();
+            document.getElementById("login").style.display="none";
+            document.getElementById("signup").style.display="block";
+        }
+        document.getElementById("login-btn").onclick = (e) => {
+            e.preventDefault();
+            doLogin();
+        }
+    }
+}
+
+const doLogin = () => {
+    const login = new API;
+    const data = {
+        "username": document.getElementById("username").value,
+        "password": document.getElementById("password").value,
+    }
+    const sendLoginOptions = {
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+    }
+    login.post("auth/login", sendLoginOptions)
+        .then((ret) => {
+            if (ret.message) {
+                alert(ret.message);
+            }
+            else {
+                // token if authenticated
+                console.log(ret)
+                // getFeed(ret);
+            }
+        });
+}
+
+export default loginPage
---
+++ src/signup.js
@@ -0,0 +1,68 @@
+"use strict";
+import API from "./api.js";
+import { renderHTML } from "./helpers.js";
+
+const signupPage = {
+    load: () => renderHTML(`
+    <form id="signup">
+        <div><input type="text" placeholder="Create username" id="user"></div>
+        <div><input type="password" placeholder="Create password" id="pw"></div>
+        <div><input type="password" placeholder="Retype password" id="check-pw"></div>
+        <div><input type="text" placeholder="Email" id="email"></div>
+        <div><input type="text" placeholder="Name" id="name"></div>
+        <div class="button-wrapper">
+            <button id="go-to-login">Back to Login</button>
+            <button type="submit" id="signup-btn">Submit</button>
+        </div>
+    </form>
+    `
+        , "signup"),
+    setEvents: () => {
+        document.getElementById("signup-btn").onclick = (e) => {
+            e.preventDefault();
+            doSignup();
+        }
+        document.getElementById("go-to-login").onclick = (e) => {
+            e.preventDefault();
+            document.getElementById("login").style.display="block";
+            document.getElementById("signup").style.display="none";
+        }
+    }
+}
+
+const doSignup = () => {
+    const password = document.getElementById("pw").value;
+    const retype = document.getElementById("check-pw").value;
+    try {
+        if (password !== retype) throw "Passwords do not match!"
+    }
+    catch (err) {
+        alert(err);
+        return;
+    }
+    const signup = new API;
+    const data = {
+        "username": document.getElementById("user").value,
+        "password": password,
+        "email": document.getElementById("email").value,
+        "name": document.getElementById("name").value,
+    }
+    const sendSignupOptions = {
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data),
+    }
+    signup.post("auth/signup", sendSignupOptions)
+        .then((ret) => {
+            if (ret.message) {
+                alert(ret.message);
+            }
+            else {
+                // token if authenticated
+                alert('signup successful!');
+                console.log(ret);
+                // return ret;
+            }
+        });
+}
+
+export default signupPage;
