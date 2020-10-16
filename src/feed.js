9120a16496506adb3e29df79e68493e7d5d43598
Debbie Cheng <dqrcheng@gmail.com>
2020-10-16 11:36:05 +1100

    like/unlike working

--- src/feed.js
+++ src/feed.js
@@ -12,7 +12,7 @@ export const stickBanner = () => {
     banner.style.top = 0;
     banner.style.position = "sticky";
     const wrapper = document.getElementById("page-wrapper");
-    wrapper.style.height="auto";
+    wrapper.style.height = "auto";
 }
 
 // get Feed after login with token
@@ -60,7 +60,7 @@ const createPost = (postId, author, time, likes, description, comments, img) => 
     const heart = newNode.getElementsByClassName("heart")[0];
     heart.onclick = (e) => {
         e.preventDefault();
-
+        isLiked(likes, postId);
     }
     const post = newNode.getElementsByClassName("post");
     feed.appendChild(post[0]);
@@ -74,7 +74,7 @@ const createPost = (postId, author, time, likes, description, comments, img) => 
 // How many comments the post has
 const showPosts = (posts) => {
     Object.keys(posts).forEach((post) => {
-         console.log(posts[post]);
+        //  console.log(posts[post]);
         if (posts[post].length === 0) {
             console.log('Oops no posts here');
             return;
@@ -101,41 +101,57 @@ const showPosts = (posts) => {
 
 document.getElementById("main").appendChild(feed);
 
+const isLiked = (likes, postId) => {
+    const userId = new API;
+    console.log(`likes arr: ${likes}`);
+    const token = {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Token ${localStorage.getItem("token")}`,
+        },
+    }
+    userId.get("user/", token)
+        .then((ret) => {
+            likes.includes(ret.id) ? removeLike(postId) : addLike(postId);
+        })
+}
 
 const addLike = (postId) => {
+    console.log('like')
     const like = new API;
     const token = {
-        headers: { 
-            "content-type": "application/json", 
+        headers: {
+            "content-type": "application/json",
             "authorization": `Token ${localStorage.getItem("token")}`,
         },
     }
     like.put(`post/like?id=${postId}`, token)
-    .then((ret) => {
-        if (ret.message) {
-            alert(ret.message);
-        }
-        else {
-            console.log(ret);
-        }
-    });
+        .then((ret) => {
+            if (ret.message) {
+                console.log(ret.message);
+            }
+            else {
+                console.log(ret);
+            }
+        });
 }
 
 const removeLike = (postId) => {
+    console.log('removeLike')
     const unlike = new API;
     const token = {
-        headers: { 
-            "content-type": "application/json", 
+        headers: {
+            "content-type": "application/json",
             "authorization": `Token ${localStorage.getItem("token")}`,
         },
     }
     unlike.put(`post/unlike?id=${postId}`, token)
-    .then((ret) => {
-        if (ret.message) {
-            alert(ret.message);
-        }
-        else {
-            console.log(ret);
-        }
-    });
+        .then((ret) => {
+            if (ret.message) {
+                console.log(ret.message);
+            }
+            else {
+                console.log(ret);
+            }
+        });
 }
