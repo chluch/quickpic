8605e40694976a25d7d97ce1b1082806075c0c36
Debbie Cheng <dqrcheng@gmail.com>
2020-10-16 21:06:51 +1100

    can live update like/unlike

--- src/likes.js
+++ src/likes.js
@@ -1,21 +1,16 @@
 "use strict";
 import API from "./api.js";
 
-
-export const handleLike = (likes, postId) => {
-    const userId = new API;
-    console.log(`likes arr: ${likes}`);
-    const token = {
-        headers: {
-            "content-type": "application/json",
-            "authorization": `Token ${localStorage.getItem("token")}`,
-        },
+export async function handleLike(postId) {
+    const getUser = new API;
+    const getPost = new API;
+    const option = {
+        headers: { "content-type": "application/json", "authorization": `Token ${localStorage.getItem("token")}` },
     }
-    userId.get("user/", token)
-        .then((ret) => {
-            console.log(likes.includes(ret.id))
-            likes.includes(ret.id) ? removeLike(postId) : addLike(postId);
-        })
+    const userData = await getUser.get("user/", option);
+    const postData = await getPost.get(`post/?id=${postId}`, option);
+    const isLiked = postData.meta.likes.includes(userData.id);
+    isLiked ? removeLike(postId) : addLike(postId);
 }
 
 const addLike = (postId) => {
@@ -29,18 +24,10 @@ const addLike = (postId) => {
     like.put(`post/like?id=${postId}`, token)
         .then(() => {
             updateLike(postId);
-            // let likesNum = document.getElementById(`likes-num-${postId}`);
-            // likesNum.innerText = parseInt(likesNum.innerText) +1;
-            // likesNum.innerText = likes.len;
-            // let post = document.getElementsByClassName("post")[0];
-            // post.appendChild(likesNum);
-            // feed.appendChild(post);
-            // document.getElementById("main").appendChild(feed);
         });
 }
 
 const removeLike = (postId) => {
-    console.log('removeLike')
     const unlike = new API;
     const token = {
         headers: {
@@ -51,12 +38,6 @@ const removeLike = (postId) => {
     unlike.put(`post/unlike?id=${postId}`, token)
         .then(() => {
             updateLike(postId);
-            // let likesNum = document.getElementById(`likes-num-${postId}`);
-            // likesNum.innerText = parseInt(likesNum.innerText) -1;
-            // let post = document.getElementsByClassName("post")[0];
-            // post.appendChild(likesNum);
-            // feed.appendChild(post);
-            // document.getElementById("main").appendChild(feed);
         });
 }
 
@@ -70,7 +51,6 @@ const updateLike = (postId) => {
     }
     setLike.get(`post/?id=${postId}`, token)
     .then((ret) => {
-        // console.log(ret.meta.likes.length);
         let likesCount = document.getElementById(`likes-num-${postId}`);
         likesCount.innerText = ret.meta.likes.length;
     })
