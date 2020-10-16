f7ba0514cfd444734bcccfd0933f6469ca5b9d11
Debbie Cheng <dqrcheng@gmail.com>
2020-10-16 15:47:19 +1100

    implement comment toggle

--- src/feed.js
+++ src/feed.js
@@ -41,22 +41,57 @@ const createPost = (postId, author, time, likes, description, comments, img) => 
         alt: `${author}'s post`
     });
 
+    // save each comment as div in array
+    let commentLog = [];
+    Object(comments).forEach((c) => {
+        // console.log(`${c.author}: ${c.comment}`);
+        const commenter = document.createElement("span");
+        commenter.className = "commenter";
+        const commentText = document.createElement("span");
+        commentText.className = "comment";
+        commenter.innerText = `${c.author}: `;
+        commentText.innerText = c.comment;
+        const wrapper = document.createElement("div");
+        wrapper.appendChild(commenter);
+        wrapper.appendChild(commentText);
+        commentLog.push(wrapper);
+    });
+
     let postTemplate = `
         <div class="post" id=post-${postId}>
-            <h2 class="author">${author}</h2>
+            <div class="post-heading">
+                <h2 class="author">${author}</h2>
+                <div class="timestamp">${getTime(time)}</div>
+            </div>
             <div class="post-img"></div>
             <div class="post-info">
                 <p class="post-text">${description}</p>
                 <div class="comments-number">${comments.length} comments</div>
                 <div class="likes-number">${likes.length} <span class="heart">&#x2764;</span></div>
-                <div class="timestamp">${getTime(time)}</div>
+                <div class="comment-display" id="comment-display-${postId}"></div>
             </div>
         </div>
     `;
+    // set post image
     const parser = new DOMParser();
     const newNode = parser.parseFromString(postTemplate, "text/html");
     const imgWrapper = newNode.getElementsByClassName("post-img");
     imgWrapper[0].appendChild(image);
+
+    // set comments
+    const commentDisplay = newNode.getElementsByClassName("comment-display")[0];
+    for (let el of commentLog) {
+        commentDisplay.appendChild(el)
+    }
+
+    const showComments = newNode.getElementById(`comment-display-${postId}`)
+    showComments.style.display = "none";
+    const commentNum =  newNode.getElementsByClassName("comments-number")[0];
+    commentNum.onclick = () => {
+        // e.preventDefault();
+        showComments.style.display === "none" ? showComments.style.display = "block" : showComments.style.display = "none";
+    }
+    // "Like" feature
     const heart = newNode.getElementsByClassName("heart")[0];
     heart.onclick = (e) => {
         e.preventDefault();
@@ -65,6 +100,8 @@ const createPost = (postId, author, time, likes, description, comments, img) => 
     const post = newNode.getElementsByClassName("post");
     feed.appendChild(post[0]);
 }
+
+document.getElementById("main").appendChild(feed);
 
 // Who the post was made by
 // When it was posted
@@ -80,6 +117,7 @@ const showPosts = (posts) => {
             return;
         }
         posts[post].forEach((p) => {
+            console.log(p.comments)
             // console.log(p.meta.author);
             // console.log(getTime(p.meta.published));
             // console.log(p.meta.likes.length);
@@ -98,8 +136,6 @@ const showPosts = (posts) => {
         })
     });
 }
-
-document.getElementById("main").appendChild(feed);
 
 const isLiked = (likes, postId) => {
     const userId = new API;
@@ -126,13 +162,13 @@ const addLike = (postId) => {
         },
     }
     like.put(`post/like?id=${postId}`, token)
-        .then((ret) => {
-            if (ret.message) {
-                console.log(ret.message);
-            }
-            else {
-                console.log(ret);
-            }
+        .then(() => {
+            let likesNum = document.getElementsByClassName("likes-number")[0];
+            console.log("eh" + likesNum.innerText.match(/\d/));
+            // let post = document.getElementsByClassName("post")[0];
+            // post.appendChild(likesNum);
+            // feed.appendChild(post);
+            // document.getElementById("main").appendChild(feed);
         });
 }
 
@@ -146,12 +182,12 @@ const removeLike = (postId) => {
         },
     }
     unlike.put(`post/unlike?id=${postId}`, token)
-        .then((ret) => {
-            if (ret.message) {
-                console.log(ret.message);
-            }
-            else {
-                console.log(ret);
-            }
+        .then(() => {
+            let likesNum = document.getElementsByClassName("likes-number")[0];
+            console.log(likesNum.innerText.match(/\d/));
+            // let post = document.getElementsByClassName("post")[0];
+            // post.appendChild(likesNum);
+            // feed.appendChild(post);
+            // document.getElementById("main").appendChild(feed);
         });
 }
