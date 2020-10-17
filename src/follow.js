89d6096f3a2bf7a0e5c33514818a90ea24a48471
Debbie Cheng <dqrcheng@gmail.com>
2020-10-17 22:27:09 +1100

    init

---
+++ src/follow.js
@@ -0,0 +1,22 @@
+"use strict";
+import API from "./api.js";
+
+export async function showFollowing (d) {
+    const data = await d;
+}
+
+function handleFollow () {
+
+}
+
+function addFollow() {
+
+}
+
+function removeFollow() {
+
+}
+
+function updateFollow(){
+
+}
